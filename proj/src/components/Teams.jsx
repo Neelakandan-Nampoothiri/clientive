be9bd30b989caf177bff64399681edc02544fb1c
Neelakandan Nampoothiri N <neelakandannampoothirin@gmail.com>
2026-06-06 22:10:34 +0530

    updated

--- proj/src/components/Teams.jsx
+++ proj/src/components/Teams.jsx
@@ -15,7 +15,7 @@ const Teams = () => {
     
     
     className='flex  flex-col items-center gap-7 px-4 sm:px-12 lg:px-24 xl:px-40 pt-30 text-gray-700 dark:text-white'>
-    <Title title='Our Expert Team' desc='A team of creators, developers, and strategists dedicated to building exceptional digital solutions.'/>
+    <Title title='Our Expert Team' desc='A team of creators,developers, and strategists dedicated to building exceptional digital solutions.'/>
 
         <div className='grid grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-5'>
             {
